fb96138c79eb4d928ae7707def3005e56c80f105
eriamuri <eriamuri@espol.edu.ec>
2025-01-13 20:13:33 -0500

    clases actividad juego

---
+++ ED_P1_Grupo02/app/src/main/java/com/example/myapplication/AddPlayers.java
@@ -0,0 +1,42 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class AddPlayers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_players);
+
+        EditText jugador1 = findViewById(R.id.player1);
+        EditText jugador2 = findViewById(R.id.player2);
+        Button startGameButton = findViewById(R.id.startGameButton);
+
+        startGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String getPlayerOneName = jugador1.getText().toString();
+                String getPlayerTwoName = jugador2.getText().toString();
+
+                if (getPlayerOneName.isEmpty() || getPlayerTwoName.isEmpty()) {
+                    Toast.makeText(AddPlayers.this, "Ingrese el nombre: ", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(AddPlayers.this, MainActivity.class);
+                    intent.putExtra("playerOne", getPlayerOneName);
+                    intent.putExtra("playerTwo", getPlayerTwoName);
+                    startActivity(intent);
+                }
+            }
+        });
+
+    }
+}
